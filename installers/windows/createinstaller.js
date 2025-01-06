801e47d90b0059c762c43918c4d5697d50fdb0d1
Tame Devs <dev@tameaps.com>
2025-01-06 14:14:18 -0800

    Fix bugs

--- installers/windows/createinstaller.js
+++ installers/windows/createinstaller.js
@@ -1,26 +1,30 @@
-const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
-const path = require('path')
+const createWindowsInstaller =
+  require("electron-winstaller").createWindowsInstaller;
+const path = require("path");
 
 getInstallerConfig()
   .then(createWindowsInstaller)
   .catch((error) => {
-    console.error(error.message || error)
-    process.exit(1)
-  })
+    console.error(error.message || error);
+    process.exit(1);
+  });
 
-function getInstallerConfig () {
-  console.log('creating windows installer')
-  const rootPath = path.join('./')
-  const outPath = path.join(rootPath, 'release-builds')
+function getInstallerConfig() {
+  const rootPath = path.join("./");
+  const outPath = path.join(rootPath, "release-builds");
 
   return Promise.resolve({
-    appDirectory: path.join(outPath, 'printing-service-win32-x64/'),
-    authors: 'Tame Apps',
+    appDirectory: path.join(outPath, "printing-service-win32-x64/"),
+    authors: "Tame Apps",
     noMsi: true,
-    outputDirectory: path.join(outPath, 'windows-installer'),
-    exe: 'printing-service.exe',
-    setupExe: 'PrintingServiceInstaller.exe',
-    setupIcon: path.join(rootPath, 'assets', 'app-icon', 'win', 'icon.ico'),
-    description: "Invoices and Orders printing configurations for different printers",
-  })
+    outputDirectory: path.join(outPath, "windows-installer"),
+    exe: "printing-service.exe",
+    setupExe: "PrintingServiceInstaller.exe",
+    setupIcon: path.join(rootPath, "assets", "app-icon", "win", "icon.ico"),
+    description:
+      "Invoices and Orders printing configurations for different printers",
+    skipUpdateIcon: true,
+    createDesktopShortcut: true, 
+    createStartMenuShortcut: true,
+  });
 }
